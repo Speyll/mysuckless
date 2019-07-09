e3c319482c4db2f88e4d5f387f3e22e1cb33ed65
Speyll <44736944+Speyll@users.noreply.github.com>
2019-07-09 20:03:19 +0100

    Add files via upload

---
+++ dmenu/config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"kakwafont:size=11"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#E5E9F0", "#163638" },
+	[SchemeSel] = { "#163638", "#88C0D0" },
+	[SchemeOut] = { "#163638", "#88C0D0" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";

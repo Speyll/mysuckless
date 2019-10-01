1dc6da4cbbc56b8093278fa7934c83dd10ec78fe
Speyll <44736944+Speyll@users.noreply.github.com>
2019-10-02 00:32:07 +0100

    Update config.def.h

--- surf/config.def.h
+++ surf/config.def.h
@@ -1,10 +1,10 @@
 /* modifier 0 means no modifier */
 static int surfuseragent    = 1;  /* Append Surf version to default WebKit user agent */
 static char *fulluseragent  = ""; /* Or override the whole user agent string */
-static char *scriptfile     = "~/.surf/script.js";
-static char *styledir       = "~/.surf/styles/";
-static char *cachedir       = "~/.surf/cache/";
-static char *cookiefile     = "~/.surf/cookies.txt";
+static char *scriptfile     = "~/.config/surf/script.js";
+static char *styledir       = "~/.config/surf/styles/";
+static char *cachedir       = "~/.config/surf/cache/";
+static char *cookiefile     = "~/.config/surf/cookies.txt";
 
 /* Webkit default features */
 static Parameter defconfig[ParameterLast] = {
@@ -29,7 +29,7 @@ static Parameter defconfig[ParameterLast] = {
 	SETB(ShowIndicators,     1),
 	SETB(SiteQuirks,         1),
 	SETB(SpellChecking,      0),
-	SETV(SpellLanguages,     ((char *[]){ "en_US", NULL })),
+	SETV(SpellLanguages,     ((char *[]){ "fr_FR", NULL })),
 	SETB(StrictSSL,          0),
 	SETB(Style,              1),
 	SETF(ZoomLevel,          1.0),

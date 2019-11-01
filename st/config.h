6240082b4a51784830b19813b38d8357362d1ffe
Speyll <44736944+Speyll@users.noreply.github.com>
2019-11-01 15:45:39 +0000

    Update config.h

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "cherry:pixelsize=12:autohint=false";
+static char *font = "cherry:pixelsize=11:autohint=false";
 static int borderpx = 25;
 
 /*

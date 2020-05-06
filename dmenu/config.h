671645aaf5e5306a05d06fc3c43720eaccfd7fe3
Lyes <44736944+Speyll@users.noreply.github.com>
2020-05-06 23:47:59 +0100

    Add files via upload

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,8 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Terminus:size=9",
-	"Siji:size=9",
+	"kakwafont:style=Regular:size=9"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -17,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 18;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words

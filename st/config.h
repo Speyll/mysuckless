ba18d6e4110975d43d72fd0da55ab3441136c709
Lyes <44736944+Speyll@users.noreply.github.com>
2020-12-22 04:07:04 +0100

    Update config.h

--- st/config.h
+++ st/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "kakwafont:pixelsize=12:antialias=false:autohint=false";
+static char *font = "-kakwa-kakwafont-medium-r-normal--12-120-72-72-c-60-iso8859-1";
+static char *font2[] = { "-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1" };
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 1;
@@ -105,39 +106,48 @@ float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+
   /* 8 normal colors */
-  [0] = "#3B4252",
-  [1] = "#BF616A",
-  [2] = "#A3BE8C",
-  [3] = "#EBCB8B",
-  [4] = "#81A1C1",
-  [5] = "#B48EAD",
-  [6] = "#88C0D0",
-  [7] = "#E5E9F0",
+  [0] = "#073642", /* black   */
+  [1] = "#dc322f", /* red     */
+  [2] = "#859900", /* green   */
+  [3] = "#b58900", /* yellow  */
+  [4] = "#268bd2", /* blue    */
+  [5] = "#d33682", /* magenta */
+  [6] = "#2aa198", /* cyan    */
+  [7] = "#eee8d5", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#4C566A",
-  [9]  = "#BF616A",
-  [10] = "#A3BE8C",
-  [11] = "#EBCB8B",
-  [12] = "#81A1C1",
-  [13] = "#B48EAD",
-  [14] = "#8FBCBB",
-  [15] = "#ECEFF4",
+  [8]  = "#002b36", /* black   */
+  [9]  = "#cb4b16", /* red     */
+  [10] = "#586e75", /* green   */
+  [11] = "#657b83", /* yellow  */
+  [12] = "#839496", /* blue    */
+  [13] = "#6c71c4", /* magenta */
+  [14] = "#93a1a1", /* cyan    */
+  [15] = "#fdf6e3", /* white   */
 
   /* special colors */
-  [256] = "#2E3440", /* background */
-  [257] = "#D8DEE9", /* foreground */
+  [256] = "#002b36", /* background */
+  [257] = "#657b83", /* foreground */
 };
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor

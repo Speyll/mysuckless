b76d17c3a2c48ac50d07e7bcd5d2523045937ac7
Lyes <44736944+Speyll@users.noreply.github.com>
2020-12-22 04:04:57 +0100

    Update config.h

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,19 +4,20 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"kakwafont:style=Regular:size=9"
+	"kakwafont:style=Regular:pixelsize=12",
+	"Siji:style=Regular:size=10"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#E5E9F0", "#2E3440" },	/* white, black */
-	[SchemeSel] = { "#2E3440", "#88C0D0" }, 	/* black, aqua */
-	[SchemeOut] = { "#E5E9F0", "#88C0D0" }, 	/* white, aqua */
+	[SchemeNorm] = { "#657b83", "#002b36" },	/* white, black */
+	[SchemeSel] = { "#002b36", "#6c71c4" }, 	/* black, magenta */
+	[SchemeOut] = { "#657b83", "#6c71c4" }, 	/* white, magenta */
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 18;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words

64bb7ec4aaa96a3d16a93f0224adb80f69ebdd80
Lyes <speyll@protonmail.com>
2021-05-06 03:55:44 +0100

    Update config.h

--- dmenu/config.h
+++ dmenu/config.h
@@ -3,9 +3,15 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+/* 
 static const char *fonts[] = {
 	"kakwafont:style=Regular:pixelsize=12",
 	"Siji:style=Regular:size=10"
+*/
+static const char *fonts[] = {
+	"IBM Plex Mono:style=Medium:pixelsize=14",
+	"Noto Emoji:pixelsize=12"
+
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
